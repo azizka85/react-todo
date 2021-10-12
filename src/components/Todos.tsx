1c7a30e4d6f3e5d3ee44288f00b17a937fcdf82e
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-10-13 00:51:25 +0600

    fix

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -43,7 +43,7 @@ export function Todos() {
             {display
               ? <TodoHeaderIcon viewBox="0 0 16 16">
                   <path 
-                    fill-rule="evenodd" 
+                    fillRule="evenodd" 
                     d={`
                         M1.646 4.646a.5.5 0 0 1 .708 0L8 
                         10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 
@@ -53,7 +53,7 @@ export function Todos() {
                 </TodoHeaderIcon>
               : <TodoHeaderIcon viewBox="0 0 16 16">
                   <path 
-                    fill-rule="evenodd" 
+                    fillRule="evenodd" 
                     d={`
                         M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 
                         .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 
